36b0771b1bfd24013456227f8142838b63cbaf2e
beyzaerrdem <66781013+beyzaerrdem@users.noreply.github.com>
2023-08-06 22:48:23 +0300

    commit

--- SipayFinalCase/Api.DataAccess/Migrations/20230806100248_seed_data.cs
+++ SipayFinalCase/Api.DataAccess/Migrations/20230806100248_seed_data.cs
@@ -10,7 +10,7 @@ namespace Api.DataAccess.Migrations
         {
             migrationBuilder.Sql("INSERT INTO public.\"Apartments\"(\r\n\t \"Block\", \"Status\", \"Type\", \"Floor\", \"ApartmentNo\")\r\n\tVALUES ( 'A', true, '2+1', 3, 10);");
             migrationBuilder.Sql("INSERT INTO public.\"UserLogin\"(\r\n    \"UserName\", \"Email\", \"Password\", \"Role\", \"LastActivity\", \"PasswordRetryCount\", \"Status\")\r\n\tVALUES ('kullanıcı', 'user@gmail.com','32141365', 'user', '2023-08-06 03:42:45.517951+03', 0, 1);");
-
+            migrationBuilder.Sql("INSERT INTO public.\"UserLogin\"(\r\n    \"UserName\", \"Email\", \"Password\", \"Role\", \"LastActivity\", \"PasswordRetryCount\", \"Status\")\r\n\tVALUES ('admin', 'admin@gmail.com','12345678', 'admin', '2023-08-06 03:42:45.517951+03', 0, 1);");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
